c10881213ed40c1d1a46f70fae3bb160575acd97
Fabian Snaith <fabian.snaith@gmail.com>
2025-08-19 09:45:30 +1000

    Add Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import {
+  Navbar as FlowbiteNavbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarToggle,
+} from 'flowbite-react';
+import { Sidebar } from './Sidebar';
+
+export const Navbar = () => {
+  return (
+    <FlowbiteNavbar fluid rounded className="relative z-100">
+      <NavbarBrand href="#">
+        <img
+          src="/favicon.svg"
+          className="mr-3 h-6 sm:h-9"
+          alt="Flowbite React Logo"
+        />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          ACME Inc
+        </span>
+      </NavbarBrand>
+      <NavbarToggle />
+      <NavbarCollapse className="h-full [&>ul]:mt-2">
+        <Sidebar className="w-full md:hidden border-t border-gray-200 dark:border-gray-700 fixed h-auto -left-[1px]" />
+      </NavbarCollapse>
+    </FlowbiteNavbar>
+  );
+};
