704f13d44686febb517a71d64124d616de229a3e
Fabian Snaith <fabian.snaith@gmail.com>
2025-08-19 15:50:26 +1000

    Add home page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import { Button } from 'flowbite-react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Sidebar } from './components/Sidebar';
+import { Home } from './pages/Home';
 
 export default function App() {
   return (
-    <>
+    <Router>
       <Navbar />
       <div className="mt-2 flex gap-2">
         <Sidebar className="hidden md:block" />
-        <div className="px-2">
-          <Button>Click me</Button>
-        </div>
+        <main className="flex-1 px-2">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </main>
       </div>
-    </>
+    </Router>
   );
 }
